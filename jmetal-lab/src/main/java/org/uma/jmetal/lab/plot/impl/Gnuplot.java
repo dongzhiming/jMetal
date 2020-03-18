bfac5ad68ccfd42376b7bf03279f0ed9c78812a4
turner <dongzm@stumail.neu.edu.cn>
2020-03-18 09:43:14 +0800

    add Gnuplot

---
+++ jmetal-lab/src/main/java/org/uma/jmetal/lab/plot/impl/Gnuplot.java
@@ -0,0 +1,57 @@
+package org.uma.jmetal.lab.plot.impl;
+
+/**
+ * @Author: Zhi-Ming Dong, dongzm@stumail.neu.edu.com
+ * @Date: created in 上午9:42 2020/3/18
+ * @Descriptiom:
+ */
+public class Gnuplot {
+//    public static void plot(double[] data, String name) {
+//        double[][] convert = new double[data.length][2];
+//        for (int i = 0; i < data.length; i++) {
+//            convert[i][0] = i;
+//            convert[i][1] = data[i];
+//        }
+//
+//        plot(convert, name);
+//    }
+//
+//    public static void plot(double[][] data, String name) {
+//        if (data[0].length > 3) {
+//            return;
+//        }
+//
+//        JavaPlot p = new JavaPlot();
+//        if (data[0].length == 3) {
+//            p = new JavaPlot(true);
+//        }
+//
+//        p.setKey(OFF);
+//        p.setTitle(name);
+//
+//        p.addPlot(data);
+//        p.plot();
+//    }
+//
+//    public static void plots(double[][] data, String name) {
+//        JavaPlot p = new JavaPlot();
+//        if (data[0].length == 3) {
+//            p = new JavaPlot(true);
+//        }
+//
+//        p.setKey(OFF);
+//        p.setTitle(name);
+//
+//        for (int i = 0; i < data[0].length; i++) {
+//            double[][] x = new double[data.length][2];
+//            for (int j = 0; j < data.length; j++) {
+//                x[j][0] = j;
+//                x[j][1] = data[j][i];
+//            }
+//
+//            p.addPlot(x);
+//        }
+//
+//        p.plot();
+//    }
+}
