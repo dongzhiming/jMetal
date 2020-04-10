21ef472c18fc012e69ad5d76ad344c84388f8287
turner <dongzm@stumail.neu.edu.cn>
2020-04-10 15:39:41 +0800

    update Filter

--- jmetal-core/src/main/java/org/uma/jmetal/util/Filter.java
+++ jmetal-core/src/main/java/org/uma/jmetal/util/Filter.java
@@ -1,8 +1,14 @@
 package org.uma.jmetal.util;
 
+import org.uma.jmetal.solution.Solution;
+import org.uma.jmetal.util.distance.Distance;
+import org.uma.jmetal.util.distance.impl.EuclideanDistanceBetweenSolutionsInObjectiveSpace;
 import org.uma.jmetal.util.point.impl.ArrayPoint;
 import org.uma.jmetal.util.point.util.distance.EuclideanDistance;
 import org.uma.jmetal.util.point.util.distance.PointDistance;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author: Zhi-Ming Dong, dongzm@stumail.neu.edu.com
@@ -12,7 +18,7 @@ import org.uma.jmetal.util.point.util.distance.PointDistance;
 public class Filter {
     public static double[][] filter(double[][] referencePoints, int expected) {
         if (referencePoints.length < expected) {
-            JMetalLogger.logger.warning("referencePoints.size() < expected.");
+            JMetalLogger.logger.warning("referencePoints.length < expected.");
             return referencePoints;
         }
 
@@ -61,7 +67,7 @@ public class Filter {
         }
 
         if (rps.length < dim) {
-            JMetalLogger.logger.warning("rps.size() < dim");
+            JMetalLogger.logger.warning("rps.length < dim");
         }
 
         // select all the other points
@@ -77,6 +83,86 @@ public class Filter {
             }
 
             rps[k] = referencePoints[index];
+
+            selected[index] = true;
+
+            for (int i = 0; i < total; i++) {
+                if (!selected[i] && dis2set[i] > disV[i][index]) {
+                    dis2set[i] = disV[i][index];
+                }
+            }
+        }
+
+        return rps;
+    }
+
+    public static List<Solution<?>> filter(List<Solution<?>> referencePoints, int expected) {
+        if (referencePoints.size() < expected) {
+            JMetalLogger.logger.warning("referencePoints.size() < expected.");
+            return referencePoints;
+        }
+
+        int dim = referencePoints.get(0).getNumberOfObjectives();
+        List<Solution<?>> rps = new ArrayList<>(expected);
+
+        int total = referencePoints.size();
+        double[] dis2set = new double[total];
+        boolean[] selected = new boolean[total];
+        for (int i = 0; i < total; i++) {
+            dis2set[i] = Double.MAX_VALUE;
+            selected[i] = false;
+        }
+
+        // distance matrix
+        Distance distance = new EuclideanDistanceBetweenSolutionsInObjectiveSpace();
+        double[][] disV = new double[total][total];
+        for (int i = 0; i < total; i++) {
+            disV[i][i] = 0.0;
+            for (int j = i + 1; j < total; j++) {
+                disV[i][j] = distance.getDistance(referencePoints.get(i), referencePoints.get(j));
+                disV[j][i] = disV[i][j];
+            }
+        }
+
+        // select extreme points
+        for (int k = 0; k < (expected > dim ? dim : expected); k++) {
+            int index = 0;
+            double fmax = -Double.MAX_VALUE;
+            for (int i = 0; i < total; i++) {
+                if (!selected[i] && referencePoints.get(i).getObjective(k) > fmax) {
+                    index = i;
+                    fmax = referencePoints.get(i).getObjective(k);
+                }
+            }
+
+            rps.add(referencePoints.get(index).copy());
+
+            selected[index] = true;
+
+            for (int i = 0; i < total; i++) {
+                if (!selected[i] && dis2set[i] > disV[i][index]) {
+                    dis2set[i] = disV[i][index];
+                }
+            }
+        }
+
+        if (rps.size() < dim) {
+            JMetalLogger.logger.warning("rps.size() < dim");
+        }
+
+        // select all the other points
+        for (int k = dim; k < expected; k++) {
+            int index = 0;
+            double fmax = -Double.MAX_VALUE;
+
+            for (int i = 0; i < total; i++) {
+                if (!selected[i] && dis2set[i] > fmax) {
+                    fmax = dis2set[i];
+                    index = i;
+                }
+            }
+
+            rps.add(referencePoints.get(index).copy());
 
             selected[index] = true;
 
